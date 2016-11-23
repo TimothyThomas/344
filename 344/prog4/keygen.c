7b17505cf09af7999607e9714f747ddb6cf9fa5b
Tim Thomas <thomasti@oregonstate.edu>
2016-11-22 21:29:48 -0500

    simplified keygen

--- 344/prog4/keygen.c
+++ 344/prog4/keygen.c
@@ -2,42 +2,47 @@
  * CS 344 Fall 2016
  * Program 4 -- keygen 
  * by Tim Thomas 
+ *
+ * Usage:   ./keygen num
+ *
+ * Note:    num should be an integer value
+ *
+ * Output:  sends to stdout num random characters consisting of letters and
+ *          spaces and an extra newline appended at the end (so num + 1 chars total).
+ *   
  */
 
 #include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
 
 
-
 int main(int argc, char *argv[]) {
 
-    int KEY_LENGTH = strtol(argv[1], NULL, 10) + 1;  // add 1 to account for '\n' at end
-    char key[KEY_LENGTH];
-    memset(key, '\0', KEY_LENGTH);
+    // Parse command line arg and convert string to integer
+    int KEY_LENGTH = strtol(argv[1], NULL, 10); 
 
     // seed random number generator
     srand(time(NULL));
 
     int i;
     char c;
-    for (i = 0; i < KEY_LENGTH - 1; i++) {
+    for (i = 0; i < KEY_LENGTH; i++) {
+
         c = rand() % 27;
-        if (c == 26) {    // make this a space
-            c = 32;       // ascii code for space
+
+        if (c == 26) {    // make c a blank space
+            c = 32; 
         }
+
         else {
-            c += 65;
+            c += 65;     // make c an uppercase letter
         }
-        key[i] = c;
+
+        printf("%c", c);
     }
     
-    key[KEY_LENGTH-1] = '\n';
-    printf("%s\n", key);
+    printf("\n");
 
     return 0;
 }
