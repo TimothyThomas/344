c51249b4b56a5b2c2c4179c83d3ba0ac7d58b64b
Tim Thomas <thomasti@oregonstate.edu>
2017-02-11 14:28:20 -0500

    minimal working server

--- 372/project1/chatserve.py
+++ 372/project1/chatserve.py
@@ -2,27 +2,31 @@ import socket
 import sys
 
 handle = 'hostA> '
+
 def main():
     host, port = '', int(sys.argv[1])
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen(1)
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
 
-            while True:
-                data = conn.recv(500).decode('utf-8')
-                if not data:
-                    break
-                print(data)
+        while True:
+            conn, addr = s.accept()
 
-                message = input("{}".format(handle))
-                if message == r'\quit':
-                    break
-                else:
-                    conn.sendall(''.join([handle, message]).encode())
+            with conn:
+                print('Connected by', addr, flush=True)
+
+                while True:
+                    data = conn.recv(500).decode('utf-8')
+                    if not data:
+                        break
+                    print(data, flush=True)
+
+                    message = input("{}".format(handle))
+                    if message == r'\quit':
+                        break
+                    else:
+                        conn.sendall(''.join([handle, message]).encode())
 
 if __name__ == '__main__':
     main()
